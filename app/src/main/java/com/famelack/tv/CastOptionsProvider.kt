92682d4eae5f798fff4d56f4570883218c075099
Alanlee1256 <alanlee1256@gmail.com>
2026-07-04 10:42:59 +0100

    Fix CastOptionsProvider: add getAdditionalSessionProviders, remove unused imports

--- app/src/main/java/com/famelack/tv/CastOptionsProvider.kt
+++ app/src/main/java/com/famelack/tv/CastOptionsProvider.kt
@@ -3,6 +3,7 @@ package com.famelack.tv
 import android.content.Context
 import com.google.android.gms.cast.framework.CastOptions
 import com.google.android.gms.cast.framework.OptionsProvider
+import com.google.android.gms.cast.framework.SessionProvider
 
 class CastOptionsProvider : OptionsProvider {
 
@@ -13,7 +14,7 @@ class CastOptionsProvider : OptionsProvider {
             .build()
     }
 
-    override fun getAdditionalOptions(context: Context): Map<String, String> {
-        return emptyMap()
+    override fun getAdditionalSessionProviders(context: Context): List<SessionProvider> {
+        return emptyList()
     }
 }
